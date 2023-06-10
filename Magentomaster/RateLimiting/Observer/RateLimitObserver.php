c555d0259e4064f113dfc99b55bbd44ff0e1db48
kushal jindal <kushaljindal92@gmail.com>
2023-06-10 02:24:14 -0500

    Add files via upload

---
+++ Magentomaster/RateLimiting/Observer/RateLimitObserver.php
@@ -0,0 +1,70 @@
+<?php
+namespace Magentomaster\RateLimiting\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Response\Http as HttpResponse;
+use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\App\Request\Http as HttpRequest;
+use Psr\Log\LoggerInterface;
+
+class RateLimitObserver implements ObserverInterface
+{
+    protected $scopeConfig;
+    protected $response;
+    protected $remoteAddress;
+    protected $messageManager;
+    protected $request;
+    protected $logger;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        HttpResponse $response,
+        RemoteAddress $remoteAddress,
+        ManagerInterface $messageManager,
+        HttpRequest $request,
+        LoggerInterface $logger
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->response = $response;
+        $this->remoteAddress = $remoteAddress;
+        $this->messageManager = $messageManager;
+        $this->request = $request;
+        $this->logger = $logger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $isEnabled = $this->scopeConfig->getValue('rate_limiting/general/enabled');
+        $requestsPerMinute = (int) $this->scopeConfig->getValue('rate_limiting/general/requests_per_minute');
+        
+        if ($isEnabled && $requestsPerMinute > 0) {
+            $ipAddress = $this->remoteAddress->getRemoteAddress();
+            $cacheKey = 'rate_limiting_' . md5($ipAddress);
+
+            $cache = \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Framework\App\CacheInterface::class);
+            $rateLimitData = $cache->load($cacheKey);
+            $rateLimitData = $rateLimitData ? unserialize($rateLimitData) : [];
+
+            $currentTime = time();
+            $rateLimitData[] = $currentTime;
+
+            // Remove expired requests
+            $rateLimitData = array_filter($rateLimitData, function ($timestamp) use ($currentTime, $requestsPerMinute) {
+                return $timestamp >= ($currentTime - 60) && $timestamp <= $currentTime;
+            });
+
+            if (count($rateLimitData) > $requestsPerMinute) {
+                $this->logger->info('Rate limit exceeded for IP: ' . $ipAddress);
+                $this->response->setHttpResponseCode(429); // Too Many Requests
+                $this->response->setBody('Rate limit exceeded. Please try again later.');
+                $this->response->sendResponse();
+                exit();
+            }
+
+            $cache->save(serialize($rateLimitData), $cacheKey, [], 60);
+        }
+    }
+}
---
+++ Magentomaster/RateLimiting/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magentomaster_RateLimiting',
+    __DIR__
+);
